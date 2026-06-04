2694e820555c004be18ea1c5559cf2d72132bf52
swoopy1999-del <swoopy1999@gmail.com>
2026-06-04 17:58:40 +0100

    Create tsconfig.js

---
+++ tsconfig.js
@@ -0,0 +1,16 @@
+{
+    "compilerOptions": {
+        "target": "ES6",
+        "strict": false,
+        "noEmit": false,
+        "skipLibCheck": true,
+        "noImplicitAny": false,
+        "outDir": "./out"
+    },
+    "include": [
+        "larp.ts"
+    ],
+    "exclude": [
+        "poopy.ts"
+    ]
+}
